32741c8dcfaa6cad7c3b8e6c999e9b1aaa4b53ee
Hiroshi Nakao <hiroshi.nakao@gmail.com>
2011-12-28 18:02:12 +0900

    日本語とフォントの設定を追加

---
+++ init.el
@@ -0,0 +1,13 @@
+; 言語を日本語にする
+(set-language-environment 'Japanese)
+; 極力UTF-8とする
+(prefer-coding-system 'utf-8)
+
+; フォント
+(create-fontset-from-ascii-font "Menlo-14:weight=normal:slant=normal" nil "menlokakugo")
+(set-fontset-font "fontset-menlokakugo"
+                  'unicode
+                  (font-spec :family "Hiragino Kaku Gothic ProN" :size 16)
+                  nil
+                  'append)
+(add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))
